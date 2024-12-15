a0197fa8b9d4343be2075828a3596cadf836bb25
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-12-15 04:17:42 +0100

    remove NgModule

--- packages/google-recaptcha/src/public-api.ts
+++ packages/google-recaptcha/src/public-api.ts
@@ -6,15 +6,9 @@ export { RecaptchaComponent } from './lib/recaptcha.component';
 export { RecaptchaDirective } from './lib/recaptcha.directive';
 export type { RecaptchaErrorParameters } from './lib/recaptcha.directive';
 export { RecaptchaLoaderService } from './lib/recaptcha-loader.service';
-export { RecaptchaModule } from './lib/recaptcha.module';
 export type { RecaptchaSettings } from './lib/recaptcha-settings';
-
-export { RecaptchaV3Module } from './lib/recaptcha-v3.module';
 export { ReCaptchaV3Service } from './lib/recaptcha-v3.service';
 export type { OnExecuteData, OnExecuteErrorData } from './lib/recaptcha-v3.service';
-
-export { RecaptchaFormsModule } from './lib/recaptcha-forms.module';
-
 export {
   RECAPTCHA_LANGUAGE,
   RECAPTCHA_BASE_URL,
