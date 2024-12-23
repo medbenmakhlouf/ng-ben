e0f0a2488cf6ae259865180539c3ac5dbc4e7672
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-12-23 16:41:01 +0100

    use providers in demo app

--- apps/showcase/src/app/app.config.ts
+++ apps/showcase/src/app/app.config.ts
@@ -1,16 +1,14 @@
-import { type ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from '@ng-ben/google-analytics';
+import { provideGoogleAnalytic, provideGoogleAnalyticRouter } from '@ng-ben/google-analytics';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(
-      NgxGoogleAnalyticsModule.forRoot('UA-17886362-3'),
-      NgxGoogleAnalyticsRouterModule.forRoot({ include: ['/page-*'] }),
-    ),
+    provideGoogleAnalytic({ trackingCode: 'UA-17886362-3' }),
+    provideGoogleAnalyticRouter({ include: ['/page-*'] }),
   ],
 };
