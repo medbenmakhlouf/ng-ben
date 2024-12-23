4284fda9627b5445cff119a405ed6ecac7f57037
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-12-23 23:09:54 +0100

    stick to only one provider

--- apps/showcase/src/app/app.config.ts
+++ apps/showcase/src/app/app.config.ts
@@ -1,14 +1,13 @@
 import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideGoogleAnalytic, provideGoogleAnalyticRouter } from '@ng-ben/google-analytics';
+import { provideGoogleAnalytic } from '@ng-ben/google-analytics';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideGoogleAnalytic({ trackingCode: 'UA-17886362-3' }),
-    provideGoogleAnalyticRouter({ include: ['/page-*'] }),
+    provideGoogleAnalytic({ trackingCode: 'UA-17886362-3' }, { include: ['/page-*'] }),
   ],
 };
