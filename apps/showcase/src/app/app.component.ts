ba88596a213d692e8e2ed5557cc3dffdd09035a9
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-12-23 15:41:29 +0100

    move demo to separate component

--- apps/showcase/src/app/app.component.ts
+++ apps/showcase/src/app/app.component.ts
@@ -1,29 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RecaptchaComponent, type RecaptchaErrorParameters, RecaptchaLoaderService } from '@ng-ben/google-recaptcha';
+import { GoogleRecaptchaDemoComponent } from './google-recaptcha-demo/google-recaptcha-demo.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: ` <app-google-recaptcha-demo /> `,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RecaptchaComponent, FormsModule],
-  providers: [RecaptchaLoaderService],
+  imports: [GoogleRecaptchaDemoComponent],
 })
-export class AppComponent {
-  siteKey = '';
-  public formModel: { captcha?: string } = {};
-  public captchaResponse = '';
-
-  public resolved(captchaResponse: string | null): void {
-    console.log(`Resolved captcha with response: ${captchaResponse}`);
-    const newResponse = captchaResponse
-      ? `${captchaResponse.substring(0, 7)}...${captchaResponse.substring(captchaResponse.length - 7)}`
-      : captchaResponse;
-    this.captchaResponse += `${JSON.stringify(newResponse)}\n`;
-  }
-
-  public onError(errorDetails: RecaptchaErrorParameters): void {
-    this.captchaResponse += `ERROR; error details (if any) have been logged to console\n`;
-    console.log(`reCAPTCHA error encountered; details:`, errorDetails);
-  }
-}
+export class AppComponent {}
